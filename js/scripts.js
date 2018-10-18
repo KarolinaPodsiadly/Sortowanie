86d425f6ff0a593d426eae10e1e433c90508e24b
Karolina <karolinapodsiadly95@gmail.com>
2018-10-18 16:48:55 +0200

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+(function() {
+
+var table = document.querySelector("#myTable"),
+    ths = table.querySelectorAll("thead th"),
+    trs = table.querySelectorAll("tbody tr"),
+    index = thsArr.indexOf(target);
+    df = document.createDocumentFragment();
+
+    function makeArray(nodeList) {
+      var arr = [];
+
+      for (var i = 0; i < nodeList.length; i++) {
+        arr.push(nodeList[i]);
+      }
+      return arr;
+    }
+
+    function sortBy(e) {
+
+      var target = e.target,
+          thsArr = makeArray(ths),
+          trsArr = makeArray(trs),
+          index = thsArr.indexOf(target);
+
+      trsArr.sort(function(a,b){
+
+        var tdA = a.children[index].textContent,
+            tdB = b.children[index].textContent;
+
+        if (tdA < tdB) {
+          return 1;
+        } else if (tdA > tdB) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+
+      trsArr.forEach(function(tr){
+        dr.appendChild(tr);
+      });
+
+      table.querySelector("tbody").appendChild(df);
+
+    }
+    for(var i=0;i<ths.length;i++) {
+      ths[i].onclick = sortBy;
+    }
+
+})();
