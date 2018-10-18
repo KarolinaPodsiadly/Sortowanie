97d76c6ce96849b80bb1d92d40b7555d04029bae
Karolina <karolinapodsiadly95@gmail.com>
2018-10-18 17:45:02 +0200

    part II - final version

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@
 
 var table = document.querySelector("#myTable"),
     ths = table.querySelectorAll("thead th"),
-    trs = table.querySelectorAll("tbody tr"),
-    index = thsArr.indexOf(target);
-    df = document.createDocumentFragment();
+    trs = table.querySelectorAll("tbody tr");
 
     function makeArray(nodeList) {
       var arr = [];
@@ -15,12 +13,22 @@ var table = document.querySelector("#myTable"),
       return arr;
     }
 
+    function clearClassName(nodeList) {
+      for(var i = 0; i <nodeList.length; i++) {
+        nodeList[i].className = "";
+      }
+    }
+
     function sortBy(e) {
 
       var target = e.target,
           thsArr = makeArray(ths),
           trsArr = makeArray(trs),
-          index = thsArr.indexOf(target);
+          index = thsArr.indexOf(target),
+          df = document.createDocumentFragment(),
+          order = (target.className === "" || target.className === "desc") ? "asc" : "desc";
+
+        clearClassName(ths);
 
       trsArr.sort(function(a,b){
 
@@ -28,18 +36,19 @@ var table = document.querySelector("#myTable"),
             tdB = b.children[index].textContent;
 
         if (tdA < tdB) {
-          return 1;
+          return order === "asc" ? -1 : 1 ;
         } else if (tdA > tdB) {
-          return -1;
+          return order === "asc" ? 1 : -1;
         } else {
           return 0;
         }
       });
 
       trsArr.forEach(function(tr){
-        dr.appendChild(tr);
+        df.appendChild(tr);
       });
 
+      target.className = order;
       table.querySelector("tbody").appendChild(df);
 
     }
